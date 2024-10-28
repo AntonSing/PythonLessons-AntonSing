53bb6d594a2751b637abe00b94ce2db7d6babffc
AntonSing <antonsingj@gmail.com>
2024-10-28 17:52:55 +0300

    123

--- 2python.py
+++ 2python.py
@@ -19,38 +19,38 @@ else:
     print('Ошибка')
 
 #Task 3
-a = int(input('Введите число от 1 до 12: '))
-if a == 12 or a == 1 or a == 2:
-   if a == 12:
-    print('Месяц: Декабрь')
-   if a == 1:
-    print('Месяц: Январь')
-   if a == 2:
-     print('Месяц: Февраль')
-   print('Время года: Зима')
-elif a == 3 or a == 4 or a == 5:
-   if a == 3:
-     print('Месяц: Март')
-   if a == 4:
-     print('Месяц: Апрель')
-   if a == 5:
-     print('Месяц: Май')
-   print('Время года: Весна')
-elif a == 6 or a == 7 or a == 8:
-   if a == 6:
-     print('Месяц: Июнь')
-   if a == 7:
-     print('Месяц: Июль')
-   if a == 8:
-     print('Месяц: Август')
-   print('Время года: Лето')
-elif a == 9 or a == 10 or a == 11:
-   if a == 9:
-     print('Месяц: Сентябрь')
-   if a == 10:
-     print('Месяц: Октябрь')
-   if a == 11:
-     print('Месяц: Ноябрь')
-   print('Время года: Осень')
+data = int(input("Введите число от 1 до 12: "))
+if data == 12 or data == 1 or data == 2:
+    if data == 12:
+        print("Месяц: Декабрь")
+    if data == 1:
+        print("Месяц: Январь")
+    if data == 2:
+        print("Месяц: Февраль")
+    print("Время года: Зима")
+elif data == 3 or data == 4 or data == 5:
+    if data == 3:
+        print("Месяц: Март")
+    if data == 4:
+        print("Месяц: Апрель")
+    if data == 5:
+        print("Месяц: Май")
+    print("Время года: Весна")
+elif data == 7 or data == 8 or data == 9:
+    if data == 7:
+        print("Месяц: Июнь")
+    if data == 8:
+        print("Месяц: Июль")
+    if data == 9:
+        print("Месяц: Август")
+    print("Время года: Лето")
+elif data == 9 or data == 10 or data == 11:
+    if data == 9:
+        print("Месяц: Сентябрь")
+    if data == 10:
+        print("Месяц: Октябрь")
+    if data == 11:
+        print("Месяц: Ноябрь")
+    print("Время года: Осень")
 else:
-  print('Ошибка')
+    print("Ошибка")
