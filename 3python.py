7624c7d3b579353f1ea640f3fcc1b5db7362d897
AntonSing <antonsingj@gmail.com>
2024-11-10 18:48:52 +0300

    123

--- 3python.py
+++ 3python.py
@@ -1,15 +1,15 @@
 #Task 1
-n = int(input('Введите число:'))
-s = 0
-if n >= 100:
-    print('Ошибка')
+up_lim = int(input("Введите чило не превышающее 100: "))
+m = 0
+if up_lim > 100:
+    print("Error")
 else:
-    for i in range(1, n + 1):
-        s += i**3
-print(s)
+    for i in range(1, up_lim + 1):
+        m += i ** 3
+print(f"Сумма кубов от 1 до {up_lim} равна:", m)
 
 #Task 2
 for i in range(1,10):
     for j in range(1,10):
-        print(f'{i * j: 3}', end=' ')
+        print(f"{i * j: 3}", end=" ")
     print()
